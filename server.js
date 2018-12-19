96d481f1ac235f17cf9de2a25b55aabfbe8b67ae
sheidaei <01@sheidaei.com>
2018-12-19 16:14:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ var port = process.env.PORT || 3000;
 app.use(express.static('public'))
 
 app.get('/', function(req, res) {
-  if((Math.floor(Math.random() * 2) + 1)>1)
-  {
-    res.sendFile(__dirname + "/public/index1.html");
-  }
     res.sendFile(__dirname + "/public/index2.html");
 });
 
