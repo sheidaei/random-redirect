eab0cb10fd86555a710d8b9986a9883ff5778e8d
sheidaei <01@sheidaei.com>
2018-12-19 16:09:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,22 +2,15 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-//app.use(express.static('public'))
+app.use(express.static('public'))
 
 app.get('/', function(req, res) {
-  res.sendFile( 'listoflinks.txt', function ( line ) { res.send( line ); } );
+  if((Math.floor(Math.random() * 2) + 1)>1)
+  {
+    res.sendFile(__dirname + "/public/index1.html");
+  }
+    res.sendFile(__dirname + "/public/index2.html");
 });
-
-function getRandomLine(filename, callback){
-var fs = require('fs');
-fs.readFile(filename, function(err, data) {
-    if(err) throw err;
-    var array = data.toString().split("\n");
-    var length = Math.floor(Math.random() * array.length) + 1 ;
-        console.log(array[length]);
-        callback (array[length]);
-});
-}
 
 /*--------------------Routing Over----------------------------*/
 
