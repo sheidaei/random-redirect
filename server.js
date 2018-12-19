c889f6ccd918a1c7ee8a65c2fe05f0750c9765c7
sheidaei <01@sheidaei.com>
2018-12-19 16:03:11 -0500

    Update server.js
    comment out public

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use(express.static('public'))
+//app.use(express.static('public'))
 
 app.get('/', function(req, res) {
   res.sendFile( 'listoflinks.txt', function ( line ) { res.send( line ); } );
